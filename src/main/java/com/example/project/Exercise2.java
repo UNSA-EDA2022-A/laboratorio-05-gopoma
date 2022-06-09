c6f5052407ef94e494e3260fcc5920d1eaac6df4
Gustavo <gordono@unsa.edu.pe>
2022-06-09 17:12:56 -0500

    Solved Exercise2

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -13,9 +13,41 @@ public class Exercise2 {
     }
 
     public boolean existenDuplicados(String str) {
-        MyStack<Character> stack = new LinkedListStack<>();
+        MyStack<Character> groupings = new LinkedListStack<>();
+        MyStack<Character> contents = new LinkedListStack<>();
         // Colocar codigo aqui
+        if(str.length() == 0)
+            return true;
 
-        return false;
+        final boolean needsNormalization = str.charAt(0) != '(' || 
+        str.charAt(str.length() - 1) != ')' || 
+        (str.charAt(0) == '(' && str.substring(1, str.length()).indexOf(')') < str.substring(1, str.length()).indexOf('(')) ||
+        (str.charAt(str.length() - 1) == ')' && str.substring(0, str.length() - 1).lastIndexOf('(') > str.substring(0, str.length() - 1).lastIndexOf(')'));
+        
+        if(needsNormalization)
+            groupings.push('(');
+        for(int i = 0; i < str.length(); i++) {
+            final char current = str.charAt(i);
+
+            if(current == '(') {
+                groupings.push('(');
+            }
+            else if(!contents.isEmpty() && current == ')') {
+                contents.pop();
+                groupings.pop();
+            } else if(current != '(' && current != ')') {
+                contents.push('*');
+                i = (str.charAt(i + 1) != '(' && str.charAt(i + 2) != '(')? i + str.substring(i + 1, str.length()).indexOf(')') : i;
+            } else {
+                return true;
+            }
+        }
+
+        if(needsNormalization && !contents.isEmpty()) {
+            contents.pop();
+            groupings.pop();
+        }
+
+        return !groupings.isEmpty();
     }
 }
